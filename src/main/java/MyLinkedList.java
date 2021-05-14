e0503d90d592c9fe50555dab47894116b0f15c2c
ayurn <65171090+ayurn@users.noreply.github.com>
2021-05-14 18:48:48 +0530

    Added : ability to insert 30 between 56 and 70

--- src/main/java/MyLinkedList.java
+++ src/main/java/MyLinkedList.java
@@ -19,6 +19,25 @@ public class MyLinkedList implements INode{
         }
     }
 
+    //UC3 APPEND DATA
+    public void append(INode newNode) {
+        if(this.head == null)
+            this.head = newNode;
+        if(this.tail == null)
+            this.tail = newNode;
+        else {
+            INode tempNode = this.tail;
+            this.tail = newNode;
+            tempNode.setNext(newNode);
+        }
+    }
+
+    //UC4 INSERT DATA IN BETWEEN
+    public void insert(INode myNode, INode newNode) {
+        this.head.setNext(myNode);
+        myNode.setNext(newNode);
+    }
+
     public void printMyNodes(){
         StringBuffer myNodes = new StringBuffer("My Nodes: ");
         INode tempNode = head;
@@ -32,17 +51,6 @@ public class MyLinkedList implements INode{
         System.out.println(myNodes);
     }
 
-    public void append(INode newNode) {
-        if(this.head == null)
-            this.head = newNode;
-        if(this.tail == null)
-            this.tail = newNode;
-        else {
-            INode tempNode = this.tail;
-            this.tail = newNode;
-            tempNode.setNext(newNode);
-        }
-    }
 
     @Override
     public Object getKey() {
